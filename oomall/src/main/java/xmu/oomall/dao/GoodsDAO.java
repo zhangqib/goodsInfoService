7682737f8dac2a07406e87b72aaadb3ed95607a6
zhang <wszqbds@gmail.com>
2019-12-13 22:58:21 +0800

    merge GoodsDAO

--- oomall/src/main/java/xmu/oomall/dao/GoodsDAO.java
+++ oomall/src/main/java/xmu/oomall/dao/GoodsDAO.java
@@ -9,6 +9,7 @@ import xmu.oomall.mapper.GoodsMapper;
 import xmu.oomall.mapper.ProductMapper;
 import xmu.oomall.util.Copyer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,22 +25,24 @@ public class GoodsDAO {
 
     /**
      * 将商品插入数据库
+     *
      * @param goods
      * @return 更新完id的商品
      */
-    public Goods insert(Goods goods) {
+    public GoodsPo insert(GoodsPo goods) {
         goodsMapper.insert(goods);
         return goods;
     }
 
     /**
      * 根据id删除Goods，级联删除其对应的product
+     *
      * @param id
      * @return 删除是否成功
      */
     public boolean deleteById(Integer id) {
         List<ProductPo> products = productMapper.selectByGoodsId(id);
-        for(ProductPo product: products) {
+        for (ProductPo product : products) {
             if (productMapper.deleteByPrimaryKey(product.getId()) == 0) {
                 return false;
             }
@@ -49,6 +52,7 @@ public class GoodsDAO {
 
     /**
      * 根据id返回对应的商品
+     *
      * @param id
      * @return Goods
      */
@@ -59,6 +63,7 @@ public class GoodsDAO {
 
     /**
      * 根据productId返回Goods
+     *
      * @param productId
      * @return productId所属的商品
      */
@@ -73,10 +78,11 @@ public class GoodsDAO {
 
     /**
      * 根据参数的id更新对应的goods
+     *
      * @param goods
      * @return 更新是否成功
      */
-    public boolean updateById(Goods goods) {
+    public boolean updateById(GoodsPo goods) {
         return goodsMapper.updateByPrimaryKey(goods) == 1;
     }
 
@@ -84,25 +90,40 @@ public class GoodsDAO {
         return true;
     }
 
-    public List<Goods> selectGoodsByCondition(String goodsSn, String name, Integer page, Integer limit) {
+    public List<GoodsPo> selectByCondition(String goodsSn, String goodsName, Integer status, Integer page, Integer limit) {
         return null;
     }
 
-    public List<Goods> selectByCategoryId(Integer id) {
-        return null;
+    public List<GoodsPo> selectByCategoryId(Integer id, Integer page, Integer limit) {
+        List<GoodsPo> goodsPos = goodsMapper.selectByCategoryId(id);
+        return goodsPos;
     }
 
     /**
      * 将GoodsPo转换成Goods对象
+     *
      * @param goodsPo
      * @return goods pojo
      */
     private Goods goods(GoodsPo goodsPo) {
         Goods goods = new Goods();
-        if (Copyer.Copy(goodsPo, goods)) {
-            return goods;
-        } else {
+        return Copyer.Copy(goodsPo, goods) ? goods : null;
+    }
+
+    /**
+     * 将goodsPo列表转换成goods类表
+     *
+     * @param goodsPoList
+     * @return goodsList
+     */
+    private List<Goods> goodsList(List<GoodsPo> goodsPoList) {
+        if (goodsPoList == null) {
             return null;
         }
+        List<Goods> goodsList = new ArrayList<Goods>();
+        for (GoodsPo goodsPo : goodsPoList) {
+            goodsList.add(goods(goodsPo));
+        }
+        return goodsList;
     }
 }
