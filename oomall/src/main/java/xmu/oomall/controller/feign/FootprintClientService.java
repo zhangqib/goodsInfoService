b5e96ea87cc419a07829caee6582f341f2332ce3
Ke <you@example.com>
2019-12-23 10:51:21 +0800

    a

--- oomall/src/main/java/xmu/oomall/controller/feign/FootprintClientService.java
+++ oomall/src/main/java/xmu/oomall/controller/feign/FootprintClientService.java
@@ -10,8 +10,7 @@ import xmu.oomall.domain.po.FootprintItemPo;
 
  */
 
-@FeignClient(value="FOOTPRINTSERVICE")
-
+@FeignClient(value="FOOTPRINTSERVICE",url = "101.200.178.99:8010")
 public interface FootprintClientService {
     @PostMapping("/footprints")
     Object addFootprint(@RequestBody FootprintItemPo footprintItemPo);
